5aa9b61261a4f412110803ac1728aedeab596d2a
flasky06 <bonnienjuguna106@gmail.com>
2024-06-17 23:24:15 +0300

    first commit

---
+++ src/test/java/com/tritva/springboot_project/SpringbootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tritva.springboot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
